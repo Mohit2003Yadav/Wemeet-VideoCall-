c7b16a8eb25caf718823dab4a05e710289f9e99c
Mohit2003Yadav <yadavmohit231079@gmail.com>
2026-05-22 21:02:00 +0530

    deployment setup

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
-const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"
-  : "http://localhost:5000";
+const server =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://your-backend-name.onrender.com";
 
 export default server;
--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,15 @@ connectToSocket(server);
 // });
 
 /* ---------- Middlewares ---------- */
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-frontend-name.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
