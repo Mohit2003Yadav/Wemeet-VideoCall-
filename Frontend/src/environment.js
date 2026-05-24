2eb1d01c32ea3eb24edfb6ea2c6fc6174332919a
Mohit2003Yadav <yadavmohit231079@gmail.com>
2026-05-24 10:04:06 +0530

    added production backend url

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,6 +1,6 @@
 const server =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "https://your-backend-name.onrender.com";
+    : "https://wemeet-videocall-backend.onrender.com";
 
 export default server;
