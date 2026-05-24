4015c2deeb25fa7e377813ce112f989bb3115a45
Mohit2003Yadav <yadavmohit231079@gmail.com>
2026-05-24 11:44:16 +0530

    fixed pages import issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
-// import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './Pages/landing';
 import Authentication from './Pages/authentication';
-import { AuthProvider } from './context/AuthContext'; // ✅ IMPORTANT
+import { AuthProvider } from './context/AuthContext';
 import VideoMeetComponent from './Pages/VideoMeet';
 import HistoryComponent from './Pages/History';
-import HomeComponent from './pages/Home';
+import HomeComponent from './Pages/Home';
 
 function App() {
   return (
@@ -15,9 +14,9 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/auth" element={<Authentication />} />
-            <Route path='/history' element={<HistoryComponent />} />
-            <Route path='/home' element={<HomeComponent />} />
-            <Route path='/:url' element={<VideoMeetComponent />} />
+            <Route path="/history" element={<HistoryComponent />} />
+            <Route path="/home" element={<HomeComponent />} />
+            <Route path="/:url" element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
       </Router>
